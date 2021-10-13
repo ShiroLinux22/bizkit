82b7ff053fe9d9f4e8674e0d406b69386b1afd53
Jack C <jack@chaker.net>
2021-10-13 17:13:53 -0400

    i hate myself

--- gateway/main.go
+++ gateway/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"log"
 	"os"
 
 	"github.com/diamondburned/arikawa/v3/gateway"
 	"github.com/diamondburned/arikawa/v3/session"
 	"github.com/joho/godotenv"
+	"github.com/streadway/amqp"
 )
 
 func failOnError(err error, msg string) {
@@ -21,13 +23,46 @@ func main() {
 	err := godotenv.Load("../.env")
 	failOnError(err, "Failed to load env")
 	token := os.Getenv("BOT_TOKEN")
+	amqpUri := os.Getenv("AMQP_URI")
+
+	// Connect to rabbitmq
+	conn, err := amqp.Dial(amqpUri)
+	failOnError(err, "Failed to connect to rabbitmq")
+	defer conn.Close()
+	
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"events",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	failOnError(err, "Failed to declare a queue")
 
 	// Create Session
 	s, err := session.New("Bot " + token)
 	failOnError(err, "Failed to create session")
 
 	s.AddHandler(func(c *gateway.MessageCreateEvent) {
-		log.Println(c.Author.Username, "sent", c.Content)
+		log.Println("LOL")
+		formatted, _ := json.Marshal(c)
+		text := string(formatted)
+
+		ch.Publish(
+			"",
+			q.Name,
+			false,
+			false,
+			amqp.Publishing{
+				ContentType: "text/plain",
+				Body: []byte(text),
+			},
+		)
 	})
 
 	// Add the needed Gateway intents.
--- info/src/index.ts
+++ info/src/index.ts
@@ -1,7 +1,23 @@
 import path from "path";
+import amqp, { Connection } from "amqplib";
 import dotenv from "dotenv";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 dotenv.config({
 	path: path.resolve(__dirname, "../../.env"),
 });
+if (!process.env.AMQP_URI) throw new Error("AMQP_URI Variable not defined");
 
-console.log("Hello World!");
+let queue = "events";
+const conn = await amqp.connect(process.env.AMQP_URI);
+const channel = await conn.createChannel();
+channel.assertQueue(queue, {
+	durable: false,
+});
+
+channel.consume(queue, (msg) => {
+	console.log(msg?.content.toString());
+});
+
+console.log("waiting for events");
